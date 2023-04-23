916f43f44e3af50afc825dbb23144d4fd55708c1
RareFonder <108814268+RareFonder@users.noreply.github.com>
2023-04-23 15:21:30 +0800

    2 files modified, 1 added, 1 deleted

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 window.onload = () => {
-  const lightBtn = document.getElementById('light-campfire')
-  
+  const lightBtn = document.getElementById('light-campfire');
+  const RoomMusic = new Audio();
+  RoomMusic.src = 'https://stackblitz.com/files/web-platform-yoksb7/github/RareFonder/Campfire/main/Dream (Ambience).mp3'
+  RoomMusic.play();
+
   lightBtn.addEventListener('click', () => {
     stones.style.visibility = 'hidden';
     campfire.style.visibility = 'visible';
+    document.body.style.background = 'white';
+    lightBtn.style.color = 'black';
+    lightBtn.style.borderColor = 'black';
   });
-};
+};  
