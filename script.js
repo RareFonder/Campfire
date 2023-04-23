6512ab8303f658b685441abe316a915afb09425a
RareFonder <108814268+RareFonder@users.noreply.github.com>
2023-04-23 19:08:26 +0800

    3 files modified

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 window.onload = () => {
-  const lightBtn = document.getElementById('light-campfire');
+  const lightBtn = document.getElementById('light-btn');
+  const actions = document.getElementById('action-log');
   const RoomMusic = new Audio();
-  RoomMusic.src = 'https://stackblitz.com/files/web-platform-yoksb7/github/RareFonder/Campfire/main/Dream (Ambience).mp3'
+  RoomMusic.src = 'https://stackblitz.com/files/web-platform-yoksb7/github/RareFonder/Campfire/main/Dream (Ambience).mp3';
   RoomMusic.play();
 
   lightBtn.addEventListener('click', () => {
@@ -10,5 +11,20 @@ window.onload = () => {
     document.body.style.background = 'white';
     lightBtn.style.color = 'black';
     lightBtn.style.borderColor = 'black';
+
+    setTimeout(() => {
+      const text = document.createElement('p');
+      text.textContent = 'You light the campfire. Your surroundings brighten and the chill in the air is gone.';
+      actions.appendChild(text);
+      setTimeout(() => {
+        text.classList.add('action-add');
+      }, 50);
+      setTimeout(() => {
+        const text2 = document.createElement('p');
+        text2.textContent = 'You spot a nearby forest. You ought to collect some wood.'
+        actions.appendChild(text2);
+        text.classList.add('action-add');
+      }, 5000);
+    }, 2000);
   });
 };  
