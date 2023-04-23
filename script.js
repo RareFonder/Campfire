3aaf04fe9a24a5022b779a4fbca8af668db9bea5
RareFonder <108814268+RareFonder@users.noreply.github.com>
2023-04-23 12:05:20 +0800

    3 files modified

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 window.onload = () => {
+  const lightBtn = document.getElementById('light-campfire')
   
-}
+  lightBtn.addEventListener('click', () => {
+    stones.style.visibility = 'hidden';
+    campfire.style.visibility = 'visible';
+  });
+};
